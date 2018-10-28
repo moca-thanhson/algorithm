c128393d73a81fbab2be2b97a0c3dd6f602d368b
moca-thanhson <phanthanhsont@gmail.com>
2018-10-28 23:54:59 +0700

    Session85 : Btvn Dijkstra

--- src/main/java/com/alex/algorithm/Session12_BinarySearch/onClass/BaiHackRndm.java
+++ src/main/java/com/alex/algorithm/Session12_BinarySearch/onClass/BaiHackRndm.java
@@ -1,5 +1,9 @@
 package com.alex.algorithm.Session12_BinarySearch.onClass;
 
+import com.alex.algorithm.NumberUtil;
+
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -9,6 +13,17 @@ public class BaiHackRndm {
 
     public static void main(final String[] args) {
         final Scanner scanner = new Scanner(System.in);
+
+        final List<Integer> lineInput = NumberUtil.convertByString(scanner.nextLine());
+        final int totalNumbers = lineInput.get(0);
+        final int difference = lineInput.get(1);
+
+        //get numbers and sort them
+        final List<Integer> lstNumbers = NumberUtil.convertByString(scanner.nextLine());
+        Collections.sort(lstNumbers);
+        for(int i = 0 ;i<lstNumbers.size(); i++) {
+
+        }
 
     }
 
